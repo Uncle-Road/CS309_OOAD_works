1c5309d5a6e8452151b39bb38eba8ae8942524c7
Uncle-Road <457894974@qq.com>
2021-12-22 06:46:55 +0800

    create DateRange.java

--- Lab/Lab14/Customer.java
+++ Lab/Lab14/Customer.java
@@ -1,4 +1,3 @@
-import java.util.Enumeration;
 import java.util.Vector;
 
 class Customer {
@@ -24,29 +23,13 @@ class Customer {
 
         for (Rental each : _rentals) {
             double thisAmount = 0;
-            
+
             //determine amounts for each line
-            switch (each.getPriceCode()) {
-            case Rental.REGULAR:
-                thisAmount += 2;
-                if (each.getDaysRented() > 2) {
-                    thisAmount += (each.getDaysRented() - 2) * 1.5;
-                }
-                break;
-            case Rental.NEW_RELEASE:
-                thisAmount += each.getDaysRented() * 3;
-                break;
-            case Rental.CHILDRENS:
-                thisAmount += 1.5;
-                if (each.getDaysRented() > 3) {
-                    thisAmount += (each.getDaysRented() - 3) * 1.5;
-                }
-                break;
-            }
+            thisAmount = getThisAmount(each, thisAmount);
             // add frequent renter points
             frequentRenterPoints ++;
             // add bonus for a two day new release rental
-            if ((each.getPriceCode() == Rental.NEW_RELEASE)
+            if ((each.getPriceCode() == Movie.NEW_RELEASE)
                     && each.getDaysRented() > 1) frequentRenterPoints++;
 
             //show figures for this rental
@@ -54,7 +37,7 @@ class Customer {
             result.append("\t").append(String.valueOf(thisAmount));
             result.append("\n");
 
-            totalAmount += thisAmount;				
+            totalAmount += thisAmount;
         }
 
         //add footer lines
@@ -63,5 +46,26 @@ class Customer {
         result.append("You earned ").append(String.valueOf(frequentRenterPoints));
         result.append(" frequent renter points");
         return result.toString();
+    }
+
+    private double getThisAmount(Rental each, double thisAmount) {
+        switch (each.getPriceCode()) {
+            case Movie.REGULAR:
+                thisAmount += 2;
+                if (each.getDaysRented() > 2) {
+                    thisAmount += (each.getDaysRented() - 2) * 1.5;
+                }
+                break;
+            case Movie.NEW_RELEASE:
+                thisAmount += each.getDaysRented() * 3;
+                break;
+            case Movie.CHILDRENS:
+                thisAmount += 1.5;
+                if (each.getDaysRented() > 3) {
+                    thisAmount += (each.getDaysRented() - 3) * 1.5;
+                }
+                break;
+        }
+        return thisAmount;
     }
 }
