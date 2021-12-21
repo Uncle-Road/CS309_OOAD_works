ada830ffcd1d71dd310bdbc0e46d0387d706ada0
Uncle-Road <457894974@qq.com>
2021-12-22 06:57:05 +0800

    Have move the amountFor method

--- Lab/Lab14/Customer.java
+++ Lab/Lab14/Customer.java
@@ -25,7 +25,7 @@ class Customer {
             double thisAmount = 0;
 
             //determine amounts for each line
-            thisAmount = getThisAmount(each, thisAmount);
+            thisAmount = each.amountFor();
             // add frequent renter points
             frequentRenterPoints ++;
             // add bonus for a two day new release rental
@@ -48,24 +48,4 @@ class Customer {
         return result.toString();
     }
 
-    private double getThisAmount(Rental each, double thisAmount) {
-        switch (each.getPriceCode()) {
-            case Movie.REGULAR:
-                thisAmount += 2;
-                if (each.getDaysRented() > 2) {
-                    thisAmount += (each.getDaysRented() - 2) * 1.5;
-                }
-                break;
-            case Movie.NEW_RELEASE:
-                thisAmount += each.getDaysRented() * 3;
-                break;
-            case Movie.CHILDRENS:
-                thisAmount += 1.5;
-                if (each.getDaysRented() > 3) {
-                    thisAmount += (each.getDaysRented() - 3) * 1.5;
-                }
-                break;
-        }
-        return thisAmount;
-    }
 }
