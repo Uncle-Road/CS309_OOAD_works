82ac906abd3bc76e6f4a8500e553bb2043baa317
Uncle-Road <457894974@qq.com>
2021-12-23 01:53:56 +0800

    complete lab14 code by 11912725

--- Lab/Lab14/TestMovieRental.java
+++ Lab/Lab14/TestMovieRental.java
@@ -8,16 +8,7 @@ public class TestMovieRental {
     Customer C1;
     Customer C2;
     Customer C3;
-    Customer C4;
 
-    //    String M1_Title;
-//    int M1_PriceCode;
-//
-//    String M2_Title;
-//    int M2_PriceCode;
-//
-//    String M3_Title;
-//    int M3_PriceCode;
     Movie M1;
     Movie M2;
     Movie M3;
@@ -27,15 +18,6 @@ public class TestMovieRental {
         C1 = new Customer("John");
         C2 = new Customer("Mary");
         C3 = new Customer("Manny");
-
-//        M1_Title = "Oz The Great and Powerful";
-//        M1_PriceCode = Movie.NEW_RELEASE;
-//
-//        M2_Title = "The Dark Knight";
-//        M2_PriceCode = Movie.REGULAR;
-//
-//        M3_Title = "Wreck-it Ralph";
-//        M3_PriceCode = Movie.CHILDRENS;
         M1 = new Movie("Oz The Great and Powerful", Movie.NEW_RELEASE);
         M2 = new Movie("The Dark Knight", Movie.REGULAR);
         M3 = new Movie("Wreck-it Ralph", Movie.CHILDRENS);
@@ -47,10 +29,6 @@ public class TestMovieRental {
         Date end1 = new Date(2013, 7, 6);
         Date end2 = new Date(2013, 7, 4);
         Date end3 = new Date(2013, 7, 5);
-
-//        C1.addRental(new Rental(M1_Title, M1_PriceCode, start, end1));
-//        C1.addRental(new Rental(M2_Title, M2_PriceCode, start, end2));
-//        C1.addRental(new Rental(M3_Title, M3_PriceCode, start, end3));
         C1.addRental(new Rental(M1, new DateRange(start, end1)));
         C1.addRental(new Rental(M2, new DateRange(start, end2)));
         C1.addRental(new Rental(M3, new DateRange(start, end3)));
@@ -69,8 +47,6 @@ public class TestMovieRental {
         Date start = new Date(2013, 7, 11);
         Date end1 = new Date(2013, 7, 12);
         Date end2 = new Date(2013, 7, 19);
-//        C2.addRental(new Rental(M1_Title, M1_PriceCode, start, end1));
-//        C2.addRental(new Rental(M3_Title, M3_PriceCode, start, end2));
         C2.addRental(new Rental(M1, new DateRange(start, end1)));
         C2.addRental(new Rental(M3, new DateRange(start, end2)));
         StringBuilder ans1 = new StringBuilder("Rental Record for Mary\n");
@@ -87,8 +63,6 @@ public class TestMovieRental {
         Date start = new Date(2013, 6, 1);
         Date end1 = new Date(2013, 6, 3);
         Date end2 = new Date(2013, 6, 2);
-//        C3.addRental(new Rental(M2_Title, M2_PriceCode, start, end1));
-//        C3.addRental(new Rental(M3_Title, M3_PriceCode, start, end2));
         C3.addRental(new Rental(M2, new DateRange(start, end1)));
         C3.addRental(new Rental(M3, new DateRange(start, end2)));
         StringBuilder ans1 = new StringBuilder("Rental Record for Manny\n");
